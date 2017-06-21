e70269d834d3b4706b8cc23e94b0855111e92a4f
horlaitheo <theo.horlait@imerir.com>
2017-06-21 11:30:37 +0200

    Ajout de la structure du java

---
+++ Java/Projet_inte/src/projet_inte/Client.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_inte;
+
+/**
+ *
+ * @author Théo
+ */
+public class Client {
+    private float longitude;
+    private float lattitude;
+    private Joueur  joueur[];
+    
+    /**
+     * Constructeur de Client
+     * @param longitude
+     * @param lattitude
+     * @param joueur 
+     */
+    public Client(float longitude, float lattitude, Joueur[] joueur) {
+        this.longitude = longitude;
+        this.lattitude = lattitude;
+        this.joueur = joueur;
+    }
+    
+    /**
+     * 
+     * @return la longitude du client
+     */
+    public float getLongitude() {
+        return longitude;
+    }
+    
+    /**
+     * Définit la longitude d'un client
+     * @param longitude 
+     */
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+    
+    /**
+     * 
+     * @return la lattitude d'un client
+     */
+    public float getLattitude() {
+        return lattitude;
+    }
+
+    /**
+     * Définit la lattitude d'un client
+     * @param lattitude 
+     */
+    public void setLattitude(float lattitude) {
+        this.lattitude = lattitude;
+    }
+    
+    
+    public void Target(){
+        
+    }
+    
+    public void Move(){
+        
+    }
+    
+    public void analyse(){
+        
+    }
+}
---
+++ Java/Projet_inte/src/projet_inte/Recette.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_inte;
+
+/**
+ *
+ * @author Théo
+ */
+public class Recette {
+    private String nom;
+    private float cout;
+    private float prix;
+    private Ingredient composition[];
+    
+    /**
+     * Constructeur
+     * @param nom
+     * @param cout
+     * @param composition 
+     */
+    public Recette(String nom, float cout, float prix, Ingredient[] composition) {
+        this.prix = prix;
+        this.nom = nom;
+        this.cout = cout;
+        this.composition = composition;
+    }
+    
+    /**
+     * 
+     * @return le prix de la recette
+     */
+    public float getPrix() {
+        return prix;
+    }
+
+    /**
+     * Définit le prix de la recette
+     * @param prix 
+     */
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+    
+    /**
+     * 
+     * @return le nom de la recette
+     */
+    public String getNom() {
+        return nom;
+    }
+    
+    /**
+     * Définit le nom de la recette
+     * @param nom 
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    /**
+     * 
+     * @return le cout de la recette
+     */
+    public float getCout() {
+        return cout;
+    }
+
+    /**
+     * Définit le cout de la recette
+     * @param cout 
+     */
+    public void setCout(float cout) {
+        this.cout = cout;
+    }
+    
+    /**
+     * 
+     * @return le tableau d'ingrédient de la recette
+     */
+    public Ingredient[] getComposition() {
+        return composition;
+    }
+    
+    /**
+     * Définit le tableau d'ingrédient de la recette
+     * @param composition 
+     */
+    public void setComposition(Ingredient[] composition) {
+        this.composition = composition;
+    }
+    
+    /**
+     * Calcul le nouveau cout de la recette
+     */
+    public void CalculCout(){
+        //TODO
+    }
+}
