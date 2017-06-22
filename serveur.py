5ff97e6db7c8e6b639d38c8a2ad79f56eb0e9b22
adrien <adrien.corre@imerir.com>
2017-06-22 15:11:18 +0200

    'the road so far'

--- serveur.py
+++ serveur.py
@@ -7,9 +7,18 @@ import json
 app = Flask(__name__)
 app.debug = True
 CORS(app)
-identifiant=[]
+
+identifiant=['adrien']
 postSales=[]
 nombre = ['toto','tata','titi']
+
+
+@app.route("/retour", methods=["GET"])
+def gettest():
+	global identifiant
+	print identifiant
+	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
+
 
 @app.route("/rdm", methods=["GET"])
 def get():
@@ -18,7 +27,7 @@ def get():
 	return json.dumps(temp),200,{'Content-Type':'application/json'}
 
 @app.route("/reset", methods=["GET"])
-def get():
+def getReset():
 	global nombre
 	temp = random.choice(nombre)
 	return json.dumps(temp),200,{'Content-Type':'application/json'}
@@ -32,12 +41,31 @@ def post():
 	return json.dumps(postSales),200,{'Content-Type':'application/json'}
 
 @app.route("/idPost",methods=["POST"])
-def post():
+def postId():
 	global  identifiant
-	postSales = request.get_json()
-	print postSales
-	return json.dumps(postSales),200,{'Content-Type':'application/json'}
+	#tmp = request.get_json()
+	tmp = request.get_data()
+	tmp = json.loads(tmp)
+	identifiant.append(tmp)
+	print identifiant
+	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
 
+@app.route("/idIsValide",methods=["POST"])
+def postIdIsValide():
+	global  identifiant
+	idvalide= request.get_data()
+	idvalide = json.loads(tmp)
+	for x in identifiant:
+		if idvalide in identifiant:
+			return json.dumps(idvalide),200,{'Content-Type':'application/json'}
+		else :continue
+	
+	print identifiant
+		
+
+#@app.route("/idGet",methods=["GET"])
+#def idGet():
+#	return "test"
 
 if __name__ == "__main__":
 	app.run()	
