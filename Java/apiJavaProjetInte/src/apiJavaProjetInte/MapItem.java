bb9574a2310285c75431c5f99f9c2c0275797830
adrien <adrien.corre@imerir.com>
2017-06-26 18:51:55 +0200

    add advanced communication java

---
+++ Java/apiJavaProjetInte/src/apiJavaProjetInte/MapItem.java
@@ -0,0 +1,9 @@
+package apiJavaProjetInte;
+import apiJavaProjetInte.Coordinates;
+public class MapItem {
+	String kind;
+	String owner;
+	Coordinates location;
+	float influance;
+
+}
---
+++ Java/apiJavaProjetInte/src/apiJavaProjetInte/PlayerInfo.java
@@ -0,0 +1,11 @@
+package apiJavaProjetInte;
+import apiJavaProjetInte.DrinkInfo;
+public class PlayerInfo {
+	
+	
+	float cash;
+	int sales;
+	float profits;
+	DrinkInfo[] drinksOffered;
+	
+}
