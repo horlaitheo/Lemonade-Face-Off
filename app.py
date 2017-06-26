36b2293775178942880e10fe958d6969e3da918e
awillaumez <aurelien.willaumez@imerir.com>
2017-06-26 08:50:37 +0200

    Changement nom html

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from pprint import pprint
-import random 
+import random
 import json
- 
+
 app = Flask(__name__)
 app.debug = True
 CORS(app)
- 
+
 identifiant=['adrien']
 postSales=[]
 nombre = ['toto','tata','titi']
- 
+
 weather = []
- 
+
 @app.route("/reset", methods=["GET"])
 def getReset():
 	global nombre
 	temp = random.choice(nombre)
 	return json.dumps(temp),200,{'Content-Type':'application/json'}
 
-@app.route("/GET/metrology", methods=["GET"])
+@app.route("/metrology", methods=["GET"])
 def getWeather():
 	tmp={"weather":"sunny"}
 	return json.dumps(tmp),200,{'Content-Type':'application/json'}
@@ -29,15 +29,15 @@ def getWeather():
 def getmap():
 	#tmp={"map"{"region":"perpignan","ranking":["Kevin","adam"],"itemsByPlayer":{"kind":"shop","owner":"Jack336","location":coordinate{"latitude":0.6,"longitude":5.7},"influance":10.8},"PlayerInfo":{"jean"{"cash":3000.50,"sales":80,"profit":100.8,"drinksOffered":["name":"Mojito","price":5.80,"hasAlcohol":True,"isCold":True]}}}}
 	return json.dumps(tmp),200,{'Content-Type':'application/json'}
- 
- 
+
+
 @app.route("/sales",methods=["POST"])
 def postSales():
  	global  postSales
  	postSales = request.get_json()
- 	print postSales
+ 	print(postSales)
 
- 
+
 @app.route("/idPost",methods=["POST"])
 def postId():
  	global  identifiant
@@ -45,9 +45,9 @@ def postId():
  	tmp = request.get_data()
  	tmp = json.loads(tmp)
  	identifiant.append(tmp)
- 	print identifiant
+ 	print(identifiant)
  	return json.dumps(identifiant),200,{'Content-Type':'application/json'}
- 
+
 @app.route("/idIsValide",methods=["POST"])
 def postIdIsValide():
  	global  identifiant
@@ -57,21 +57,21 @@ def postIdIsValide():
  		if idvalide in identifiant:
  			return json.dumps(idvalide),200,{'Content-Type':'application/json'}
  		else :continue
- 	
- 	print identifiant
- 
-@app.route("/POST/meterology", methods=["POST"])
+
+ 	print(identifiant)
+
+@app.route("/meterology", methods=["POST"])
 def postWheather():
  	global weather
  	tmp = request.get_data()
  	weather.append(tmp)
- 	print weather
+ 	print(weather)
  	return json.dumps(weather),200,{'Content-Type':'application/json'}
- 		
- 
+
+
 #@app.route("/idGet",methods=["GET"])
 #def idGet():
 #	return "test"
- 
+
 if __name__ == "__main__":
- 	app.run() 
+ 	app.run()
