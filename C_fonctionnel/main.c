17c654725741dbc61782db6fa4703188698701fb
Zekiels <mama.du33@live.fr>
2017-06-27 18:26:37 +0200

    ajout de commentaire sur le c fonctionnel
    Signed-off-by: Zekiels <mama.du33@live.fr>

--- C_fonctionnel/main.c
+++ C_fonctionnel/main.c
@@ -5,12 +5,9 @@
 #include <termios.h>
 #include <curl/curl.h>
 
-
-
-
-
 int main(int argc, char *argv[])
 {
+    //ouverture du port pour se connecter a l'arduino
     int fd;
     fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
     printf("fd opened as %i\n", fd);
@@ -20,19 +17,19 @@ int main(int argc, char *argv[])
     __uint32_t tik;
     char curent_weather[10],prevision_weather[10];
     do {
-        char* buf;
+        char* buf; //redeclaration d'un buffeur pour pouvoir faire des malloc dans le do while
         buf = malloc(32);
         CURL *curl;
         CURLcode res;
-        /* In windows, this will init the winsock stuff */
+        //Initialisation de CURL_GLobal
         curl_global_init(CURL_GLOBAL_ALL);
-        /* get a curl handle */
+        //Initialisation de CURL_easy
         curl = curl_easy_init();
         struct curl_slist *list = NULL;
 
-        if(fd != -1) {
+        if(fd != -1) {//si la connection est etablie
 
-            /* Receive string from Arduino */
+            //Lecture du buffeur
             n = read(fd, buf, 64);
             if (*buf != '\n') {
                 buf[n] = 0;
@@ -42,27 +39,27 @@ int main(int argc, char *argv[])
                 k = 0;
 
                 while (buf[i] != '\n') {
-                    if (buf[i] == ' ') {
+                    if (buf[i] == ' ') { //si on rencontre un espace on passe au mot suivant
                         compt++;
                     }
                     switch (compt) {
-                        case 1:
+                        case 1:         //si c'est le premier mot
                             if (buf[i] != ' ') {
                                 curent_weather[j] = buf[i];
                                 j++;
                             }
                             break;
-                        case 2:
+                        case 2:         //si c'est le second mot
                             if (buf[i] != ' ') {
                                 prevision_weather[k] = buf[i];
                                 k++;
                             }
                             break;
                     }
-                    buf[i] = 0;
+                    //buf[i] = 0;
                     i++;
                 }
-                tik++;
+                tik++; //on compte le nombre d'occurence
 
                 printf("\n");
                 printf("%d", tik);
@@ -70,24 +67,25 @@ int main(int argc, char *argv[])
                 printf("%s", prevision_weather);
                 printf("\n");
 
-                char *msg_json;
+                char *msg_json;//on creer un message json
                 msg_json = malloc(1024);
                 sprintf(msg_json,
                         "{\"timestamp\":%d,\"weather\":[{\"dfn\":0,\"weather\":\"%s\"},{\"dfn\":1,\"weather\":\"%s\"}]}", tik,
                         curent_weather, prevision_weather);
                 if (curl) {
-                    /* Specify the ULR target */
+                    //Specifie la route sur la quelle on POST
                     curl_easy_setopt(curl, CURLOPT_URL, "https://limonade-equipe7.herokuapp.com/metrology");
-                    /* Now specify the POST data */
+                    //On specifie le message a envoyer
                     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, msg_json);
                     list = curl_slist_append(list, "content-Type:application/json");
                     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
-                    /* Perform the request, res will get the return code */
+                    //Recupere le message de retour pour verifier si le code a bien ete recu
                     res = curl_easy_perform(curl);
                     if (res != CURLE_OK) {
                         printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
                     }
                     printf("\n");
+                    //on remet a zero toutes les valeurs
                     free(msg_json);
                     free(buf);
                     curl_easy_cleanup(curl);
